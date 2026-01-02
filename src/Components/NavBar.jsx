8a5c0563729cd799782077dc85a7787e92abdded
ctian157 <violley12@gmail.com>
2026-01-02 14:47:18 -0500

    fixed NavBar in FavoritesPage; fallback language to last visited

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,36 @@
 import { Link, useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
 import './NavBar.css'
 
 function NavBar() {
     const {lang} = useParams(); //'zh', 'en' etc
 
+    //when we navigate to Explore or Home from global Favorites (no lang defined), 
+    //will use last "cached" language and navigate to that page
+    const [lastLang, setLastLang] = useState(() => { 
+        return (
+            localStorage.getItem("lastLang") || null
+        );
+    })
+
+    //if a language-required path is called:
+    useEffect(() => { 
+        if (lang) { 
+            localStorage.setItem("lastLang", lang); //update localStorage
+            setLastLang(lang); //trigger React re-render by changing state
+        }},[lang]);
+
+    //lastLang is the fallback
+    const currLang = lang || lastLang;
+
     return (
        <div className = "nav-bar">
             <Link to="/">Main</Link> {/* Language selection / main */}
-            {lang && <Link to={`/${lang}/`}>Home</Link>}
-            {lang && <Link to={`/${lang}/explore`}>Explore</Link>}
-            {lang && <Link to={`/favorites`}>Favorites</Link>}
+            {<Link to={`/${currLang}/`}>Home</Link>}
+            {<Link to={`/${currLang}/explore`}>Explore</Link>}
+            {<Link to={`/favorites`}>Favorites</Link>}
        </div> 
-    )
+    );
 
 }
 
